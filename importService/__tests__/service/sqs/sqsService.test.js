38a6ea335f3a59f391ec0a7bcddcd393327c8f64
aleksei-bulgak <aleksei.bulgak@gmail.com>
2020-11-23 23:08:37 +0300

    [test] add tests for sqs service

--- importService/__tests__/service/sqs/sqsService.test.js
+++ importService/__tests__/service/sqs/sqsService.test.js
@@ -1,45 +1,49 @@
 import AwsSdkMock from 'aws-sdk-mock';
 
-const wrapper = async (data, owner) => {
-  const snsService = await (await import('../../../src/service/sns')).default;
-  return await snsService(data, owner);
+const wrapper = async (data) => {
+  process.env.REGION = 'test';
+  process.env.SQS_TOPIC = 'test';
+  const { postDataToSqs } = await import('../../../src/service/sqs');
+  return await postDataToSqs(data);
 };
 
-describe('sns', () => {
-  const snsProcessing = jest.fn().mockImplementationOnce((params, callback) => {
+describe('sqs', () => {
+  const testQueueUrl = 'arn:testQueueUrl';
+  const sqsProcessing = jest.fn().mockImplementationOnce((params, callback) => {
     callback(null, 'success');
   });
   beforeEach(() => {
-    AwsSdkMock.mock('SNS', 'publish', snsProcessing);
+    AwsSdkMock.mock('SQS', 'sendMessage', sqsProcessing);
+    AwsSdkMock.mock('SQS', 'getQueueUrl', (_, callback) =>
+      callback(null, { QueueUrl: testQueueUrl })
+    );
     AwsSdkMock.mock('config', 'update', () => {});
   });
 
   afterEach(() => {
     jest.resetAllMocks();
-    AwsSdkMock.restore('SNS', 'publish');
+    AwsSdkMock.restore('SQS', 'sendMessage');
   });
 
   test('success case when developer attribut is set', async () => {
     const dataToPost = { test: 'some test message' };
-    const owner = 'developer';
 
-    expect(await wrapper(dataToPost, owner)).toEqual('success');
-    expect(snsProcessing.mock.calls[0][0]).toEqual({
-      Message: JSON.stringify(dataToPost),
-      MessageAttributes: { owner: { DataType: 'String', StringValue: owner } },
-      TopicArn: 'test',
+    await wrapper([dataToPost]);
+    expect(sqsProcessing.mock.calls[0][0]).toEqual({
+      DelaySeconds: 0,
+      MessageBody: JSON.stringify(dataToPost),
+      QueueUrl: testQueueUrl,
     });
   });
 
-  test('when no data specified then expect exception', async () => {
+  test('when message was not specified then expect exception', async () => {
     const dataToPost = undefined;
-    const owner = 'developer';
 
     try {
-        await wrapper(dataToPost, owner);
-        expect(false).toEqual(true);
-    } catch(err) {
-        expect(err.message).toEqual("Missing required key 'Message' in params");
+      await wrapper([dataToPost]);
+      expect(false).toEqual(true);
+    } catch (err) {
+      expect(err.message).toEqual("Missing required key 'MessageBody' in params");
     }
   });
 });

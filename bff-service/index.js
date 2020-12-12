20648def13446c859e85fe093b2dc7895c222bba
aleksei-bulgak-study <bulgak.aleksei@yandex.ru>
2020-12-12 16:20:50 +0300

    [task9] update express service to handle incoming requests

--- bff-service/index.js
+++ bff-service/index.js
@@ -1,4 +1,23 @@
 import express from 'express';
+import axios from 'axios';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const getUrlByServiceName = (serviceName) => {
+  const serviceUrl = process.env[serviceName];
+  if (!serviceUrl) {
+    throw new Error('Cannot process request');
+  }
+  return serviceUrl;
+};
+
+const getRequestBody = (req) => {
+  if(['POST', 'PUT', 'DELETE', 'PATCH'].includes(req.method)) {
+    return req.body;
+  }
+  return;
+};
 
 const port = process.env.PORT || '3000';
 const app = express();
@@ -7,21 +26,31 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.all('/:service*', (req, res, next) => {
-  console.log(req);
-  const fullUrl = req.originalUrl;
-  const serviceName = req.params.service;
-  fullUrl.replace(`/\/${serviceName}/`, '');
-  if (serviceName === 'cart') {
-    res.json({
-      fullUrl: req.originalUrl,
-      service: req.params.service,
-      other: fullUrl.replace(`\/${serviceName}`, ''),
-      body: req.body,
-      method: req.method
-    });
-    return;
+  try {
+    const serviceName = req.params.service;
+    const serviceUrl = getUrlByServiceName(serviceName);
+    const fullUrl = req.originalUrl;
+    const resultUrl = serviceUrl + fullUrl.replace(`\/${serviceName}`, '');
+
+    console.log(resultUrl);
+
+    axios({
+      method: req.method,
+      url: resultUrl,
+      data: getRequestBody(req.body),
+      headers: { ...req.headers, host: 'kutdlurimk.execute-api.eu-west-1.amazonaws.com' },
+    })
+      .then((response) => {
+        console.log('response');
+        res.status(response.status).send(response.data);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.status(err.response.status).send(err.response.data);
+      });
+  } catch (err) {
+    next();
   }
-  next();
 });
 
 app.use((req, res) => {
